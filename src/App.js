9ebbe350778f04a308541e42f91e21c064fdca8e
Annelisa Moody <am@apeiron.io>
2019-12-06 15:45:26 -0800

    testing modules

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import SimpleMenu from './components/menu.js';
+import MainTing from './components/MainTing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <MainTing />
+    <SimpleMenu />
     </div>
   );
 }
